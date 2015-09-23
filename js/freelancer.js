9ef4bda29efba16337ee586049d8764f9ef11eeb
delor34n <sebastian.lastra@usach.cl>
2015-09-23 02:23:44 -0300

    second approach: landing page with parallax scroll background images, leaflet map, pictures.

--- js/freelancer.js
+++ js/freelancer.js
@@ -35,3 +35,29 @@ $('body').scrollspy({
 $('.navbar-collapse ul li a').click(function() {
     $('.navbar-toggle:visible').click();
 });
+
+var map;
+var ajaxRequest;
+var plotlist;
+var plotlayers=[];
+
+function initmap() {
+    // set up the map
+    map = new L.Map('map');
+
+    // create the tile layer with correct attribution
+    var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+    var osmAttrib='Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
+    var osm = new L.TileLayer(osmUrl, {minZoom: 8, maxZoom: 12, attribution: osmAttrib});       
+
+    // start the map in South-East England
+    map.setView(new L.LatLng(-33.4500, -70.5700),9);
+    map.addLayer(osm);
+
+    var popup = L.popup()
+                .setLatLng([-33.4000, -70.6650])
+                .setContent("¡Trabajamos en todo chile!")
+                .openOn(map);
+}
+
+initmap();
